4badd925c76ec0ddd657f717080e0ddcca25a849
Kushagra Dwivedi <kishudwivedi30@gmail.com>
2024-12-27 01:04:45 +0530

    auction

---
+++ day9/auction.py
@@ -0,0 +1,17 @@
+name = input("What is your name?")
+bid = input("How much do you wanna bid?")
+more = input("Do we have any more bidders?")
+
+if more == "no":
+    values = {}
+    for value in values:
+        values += [name]
+        
+    print(values)
+
+
+elif more == "yes":
+    print(" \n"*100)
+    name = input("What is your name?")
+    bid = input("How much do you wanna bid?")
+    more = input("Do we have any more bidders?")
