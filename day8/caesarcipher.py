4bd3e69cfb3220afb47237ec77da22a752e9e372
Kushagra Dwivedi <kishudwivedi30@gmail.com>
2024-12-26 23:11:36 +0530

    day8

---
+++ day8/caesarcipher.py
@@ -0,0 +1,5 @@
+alphabets = [chr(i) for i in range(ord('a'), ord('z') + 1)]
+direction = input("Type 'encode to encode and 'decrypt' to decrypt: \n")
+text = input("Type your message: \n")
+shift = int(input("Type the shift number: \n"))
+
