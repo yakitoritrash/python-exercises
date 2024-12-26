deed704dcca4906bd245270c49a5244a82125c9a
Kushagra Dwivedi <kishudwivedi30@gmail.com>
2024-12-27 00:39:39 +0530

    cipher

--- day8/caesarcipher.py
+++ day8/caesarcipher.py
@@ -6,19 +6,23 @@ shift = int(input("Type the shift number: \n"))
 def encode(original_text, shift_amount):
     encrypted_text = ""
     for letter in original_text:
-        
-        alphabets.index(letter)
-        
-        new_index= alphabets.index(letter) + shift_amount
-        encrypted_text += alphabets[new_index]
+        if letter in alphabets:    
+            alphabets.index(letter)
+            
+            new_index= (alphabets.index(letter) + shift_amount) % 26
+            encrypted_text += alphabets[new_index]
+        else:
+            encrypted_text += letter
     print(encrypted_text)
   
 def decrypt(encrypted_text, shift_amount):
     decrypted_text = ""
     for letter in encrypted_text:
-        new_index = alphabets.index(letter) - shift_amount
-        decrypted_text += alphabets[new_index]
-        
+        if letter in alphabets:
+            new_index = (alphabets.index(letter) - shift_amount) % 26
+            decrypted_text += alphabets[new_index]
+        else:
+            encrypted_text += letter
     print(decrypted_text)
         
       
