15f275722c95bf82f93559d123f6bda9c6fdfd89
Kushagra Dwivedi <kishudwivedi30@gmail.com>
2025-01-03 00:47:45 +0530

    leap done

--- day10/leap.py
+++ day10/leap.py
@@ -1,6 +1,8 @@
-def is_leap_year(year):
-    if year%4 == 0:
+def is_leap_year():
+    year = int(input("What year do you want to check? \n"))
+    if year%4 == 0 and year%100 != 100 or year%400 == 0:
         print("True")
-    elif year%4 == 0 && 
+    else:
+        print("False")
 
-
+is_leap_year()
