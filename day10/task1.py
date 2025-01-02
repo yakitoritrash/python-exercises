00a1c7e5345a0441eb87612557e0eb5a627e5733
Kushagra Dwivedi <kishudwivedi30@gmail.com>
2025-01-03 01:02:21 +0530

    task1

--- day10/task1.py
+++ day10/task1.py
@@ -1,3 +1,7 @@
 def format_name(fname, lname):
     formated_fname = fname.title()
     formated_lname  = lname.title()
+
+    return f"{formated_fname} {formated_lname}"
+formatted_name = format_name("abbey", "jackson")
+print(formatted_name)
