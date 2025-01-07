d5f4023c2f50a90abfc40885cfd1fd38a7651132
Kushagra Dwivedi <kishudwivedi30@gmail.com>
2025-01-07 23:04:56 +0530

    calc_Dir

--- day10/calc_dic.py
+++ day10/calc_dic.py
@@ -17,20 +17,23 @@ operations = {
         "-": sub,
         "*": mul,
         "/": div,
-}
+        }
 
+should_accumulate = True
 num1 = float(input("What is the first number? "))
-for symbol in operations:
-    print(symbol)
-operation_symbol = input("Pick an operation: ")
-num2 = float(input("What is the next number? "))
-answer = operations[operation_symbol](num1, num2)
-print(f"{num1} {operation_symbol} {num2} = {answer}")
 
-choice = input(f"Type 'y' to continue calculating with {answer} or if you want to start with a new number type 'n'. ")
+while should_accumulate:
+    for symbol in operations:
+        print(symbol)
+    operation_symbol = input("Pick an operation: ")
+    num2 = float(input("What is the next number? "))
+    answer = operations[operation_symbol](num1, num2)
+    print(f"{num1} {operation_symbol} {num2} = {answer}")
 
-if choice == 'y':
-    num1 = answer
-    print(num1)
+    choice = input(f"Type 'y' to continue calculating with {answer} or if you want to start with a new number type 'n'. ")
 
-
+    if choice == 'y':
+        num1 = answer
+    elif choice == 'n':
+        print("\n" * 20)
+        num1 = float(input("What is the first number? "))
