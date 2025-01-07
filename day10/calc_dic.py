3250c36a6e556df696f57f3b9b19b55701bb2819
Kushagra Dwivedi <kishudwivedi30@gmail.com>
2025-01-07 22:32:35 +0530

    calc_dir

--- day10/calc_dic.py
+++ day10/calc_dic.py
@@ -26,3 +26,11 @@ operation_symbol = input("Pick an operation: ")
 num2 = float(input("What is the next number? "))
 answer = operations[operation_symbol](num1, num2)
 print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+choice = input(f"Type 'y' to continue calculating with {answer} or if you want to start with a new number type 'n'. ")
+
+if choice == 'y':
+    num1 = answer
+    print(num1)
+
+
