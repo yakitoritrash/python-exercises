5e46b440290b541ae42df3b544193728e3df99a4
Kushagra Dwivedi <kishudwivedi30@gmail.com>
2025-01-07 23:46:14 +0530

    calc_dir

--- day10/calc_dic.py
+++ day10/calc_dic.py
@@ -19,21 +19,26 @@ operations = {
         "/": div,
         }
 
-should_accumulate = True
-num1 = float(input("What is the first number? "))
+def calculate():
+    should_accumulate = True
+    num1 = float(input("What is the first number? "))
 
-while should_accumulate:
-    for symbol in operations:
-        print(symbol)
-    operation_symbol = input("Pick an operation: ")
-    num2 = float(input("What is the next number? "))
-    answer = operations[operation_symbol](num1, num2)
-    print(f"{num1} {operation_symbol} {num2} = {answer}")
+    while should_accumulate:
+        for symbol in operations:
+            print(symbol)
+        operation_symbol = input("Pick an operation: ")
+        num2 = float(input("What is the next number? "))
+        answer = operations[operation_symbol](num1, num2)
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
 
-    choice = input(f"Type 'y' to continue calculating with {answer} or if you want to start with a new number type 'n'. ")
+        choice = input(f"Type 'y' to continue calculating with {answer} or if you want to start with a new number type 'n'. ")
 
-    if choice == 'y':
-        num1 = answer
-    elif choice == 'n':
-        print("\n" * 20)
-        num1 = float(input("What is the first number? "))
+        if choice == 'y':
+            num1 = answer
+        elif choice == 'n':
+            print("\n" * 30)
+            should_accumulate = False
+            #num1 = float(input("What is the first number? ")
+            calculate()
+
+calculate()
