b9eb403846aa3dffcfcb160e218d9f43b4e0025b
Kushagra Dwivedi <kishudwivedi30@gmail.com>
2025-01-08 20:19:20 +0530

    blackjack

--- day11/blackjack.py
+++ day11/blackjack.py
@@ -1,12 +1,34 @@
-def main():
-    print("Hello.")
+import random
+
+def deal_cards():
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    card = random.choice(cards)
+    return card
+    
+
+
+user_card = []
+computer_card = []
+
+for _ in range(2):
+    user_card.append(deal_cards())
+    computer_card.append(deal_cards())
+
+
+
+start = input("Hey! do you want to play blackjack, type 'y' or 'n'? ")
+
+
+if start == 'y':
+    user1 = int(user_card[0])
+    user2 = int(user_card[1])
+    print(f"Your cards are: {user_card} and your score is {user1 + user2}. ")
+    print(f"Computer's card: [{computer_card[0]}]")
 
 
 
 
 
 
-
-
-if __name__ == "__main__":
-    main()
+#if __name__ == "__main__":
+#    main()
