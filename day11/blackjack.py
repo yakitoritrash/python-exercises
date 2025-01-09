231b16d0a4f12c94e45ae5d83c51628448fdce3c
Kushagra Dwivedi <kishudwivedi30@gmail.com>
2025-01-09 17:30:45 +0530

    blackjack

--- day11/blackjack.py
+++ day11/blackjack.py
@@ -6,14 +6,6 @@ def deal_cards():
     return card
     
 
-
-user_card = []
-computer_card = []
-
-for _ in range(2):
-    user_card.append(deal_cards())
-    computer_card.append(deal_cards())
-
 def calculate_score(cards):
     if sum(cards) == 21 and len(cards) == 2:
         return 0
@@ -22,21 +14,26 @@ def calculate_score(cards):
         cards.append(1)
     return sum(cards)
 
+user_card = []
+computer_card = []
+is_game_over = False
+
+for _ in range(2):
+    user_card.append(deal_cards())
+    computer_card.append(deal_cards())
+
+
+user_score = calculate_score(user_card)
+computer_score = calculate_score(computer_card)
+
+if user_score == 0 or computer_score == 0 or user_score > 21:
+    is_game_over = True
+
+
 
 start = input("Hey! do you want to play blackjack, type 'y' or 'n'? ")
 
 if start == 'y':
-    deal_cards()
-    user_score = calculate_score(user_card)
-    computer_score = calculate_score(computer_card)
-    print(user_score)
-    print(computer_score)
-    print(user_card)
-    print(computer_card)
-
+    print(f"Your cards: {user_card}, current score: {user_score}. \nComputer's first card: {computer_card[0]}. \nDo you want to deal another card?")
 #else:
     #    exit
-
-
-
-
