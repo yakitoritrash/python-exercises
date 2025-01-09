e74586e6754d235b9b7d2b6abdeddc8ac32c189a
Kushagra Dwivedi <kishudwivedi30@gmail.com>
2025-01-09 23:07:29 +0530

    blackjack

--- day11/blackjack.py
+++ day11/blackjack.py
@@ -22,18 +22,22 @@ for _ in range(2):
     user_card.append(deal_cards())
     computer_card.append(deal_cards())
 
-
+while not is_game_over:
 user_score = calculate_score(user_card)
 computer_score = calculate_score(computer_card)
 
 if user_score == 0 or computer_score == 0 or user_score > 21:
     is_game_over = True
+else:
+    another_round = input("Do you want to draw another card? type 'y' or 'n'. ")
+    if another_round == 'y':
+        user_card.append(deal_cards())
+    else:
+        is_game_over = True
 
+#start = input("Hey! do you want to play blackjack, type 'y' or 'n'? ")
 
-
-start = input("Hey! do you want to play blackjack, type 'y' or 'n'? ")
-
-if start == 'y':
-    print(f"Your cards: {user_card}, current score: {user_score}. \nComputer's first card: {computer_card[0]}. \nDo you want to deal another card?")
+#if start == 'y':
+#    print(f"Your cards: {user_card}, current score: {user_score}. \nComputer's first card: {computer_card[0]}.")
 #else:
     #    exit
