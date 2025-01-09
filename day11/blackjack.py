99fba031dd99af2594d9712baa7a7ad4d6bd288d
Kushagra Dwivedi <kishudwivedi30@gmail.com>
2025-01-10 01:52:16 +0530

    blackjack

--- day11/blackjack.py
+++ day11/blackjack.py
@@ -5,7 +5,6 @@ def deal_cards():
     card = random.choice(cards)
     return card
     
-
 def calculate_score(cards):
     if sum(cards) == 21 and len(cards) == 2:
         return 0
@@ -22,20 +21,24 @@ for _ in range(2):
     user_card.append(deal_cards())
     computer_card.append(deal_cards())
 
-while not is_game_over:
-user_score = calculate_score(user_card)
-computer_score = calculate_score(computer_card)
 
-if user_score == 0 or computer_score == 0 or user_score > 21:
-    is_game_over = True
-else:
-    another_round = input("Do you want to draw another card? type 'y' or 'n'. ")
-    if another_round == 'y':
-        user_card.append(deal_cards())
-    else:
-        is_game_over = True
+start = input("Hey! do you want to play blackjack, type 'y' or 'n'? ")
 
-#start = input("Hey! do you want to play blackjack, type 'y' or 'n'? ")
+if start == 'y': 
+
+
+    while not is_game_over:
+        user_score = calculate_score(user_card)
+        computer_score = calculate_score(computer_card)
+
+        if user_score == 0 or computer_score == 0 or user_score > 21:
+            is_game_over = True
+        else:
+            another_round = input("Do you want to draw another card? type 'y' or 'n'. ")
+            if another_round == 'y':
+                user_card.append(deal_cards())
+            else:
+                is_game_over = True
 
 #if start == 'y':
 #    print(f"Your cards: {user_card}, current score: {user_score}. \nComputer's first card: {computer_card[0]}.")
