cd2f63f076dc09c9d324c2c4e298a6a6e0852ffc
Arch me <kishudwivedi30@gmail.com>
2025-03-13 17:18:32 +0530

    kdsjksd

--- questions/twocrystalballs.py
+++ questions/twocrystalballs.py
@@ -1,2 +1,23 @@
-def crystalballs():
+from math import sqrt
+
+def crystalballs(breaks):
+    jmpAmount = int(sqrt(len(breaks)))
+    i = jmpAmount
+    while i < len(breaks):
+        if breaks[i]:
+            break
+        i += jmpAmount
+
+    i -= jmpAmount
+
+    for j in range(jmpAmount):
+        if i < len(breaks) and breaks[i]:
+                return i
+        i += 1
+
+    return -1z
+
+breaks = [False] * 12 + [True] * 11
+
+print(crystalballs(breaks))
 
