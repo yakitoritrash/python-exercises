42d4ae4f3b65fedeb75d4ccd723cce1af5ff9261
Arch me <kishudwivedi30@gmail.com>
2025-03-15 18:14:18 +0530

    sdgdfg

--- questions/recursioneg1.py
+++ questions/recursioneg1.py
@@ -1,9 +1,9 @@
 def foo(n):
-    if n == 1:
+    if n == 1:  #base case
         return 1
     
-    out = n + foo(n-1)
-    print(n)
+    out = n + foo(n-1)  #recurse
+    print(n)    #post  
     return out
 
 
