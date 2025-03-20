1ab8055f888e6354bb87a023cf4027ed9597a361
Arch me <kishudwivedi30@gmail.com>
2025-03-20 17:03:23 +0530

    dfkjhg

---
+++ grind/quicksort3.py
@@ -0,0 +1,38 @@
+def quicksort(arr):
+    qs(arr, 0, len(arr) - 1)
+    return arr
+
+def partition(arr, lo, hi):
+
+    pivot = arr[hi]
+    idx = lo - 1
+
+    for i in range(lo, hi):
+        if arr[i] < pivot:
+            idx += 1
+            tmp = arr[i]
+            arr[i] = arr[idx]
+            arr[idx] = tmp
+
+    idx += 1
+    arr[hi] = arr[idx]
+    arr[idx] = pivot
+
+    return idx
+
+def qs(arr, lo, hi):
+    if lo >= hi:
+        return
+
+
+    pivotIdx = partition(arr, lo, hi)
+
+    qs(arr, lo, pivotIdx - 1)
+    qs(arr, pivotIdx + 1, hi)
+
+
+arr = [5, 6, 9, 0, 4, 2]
+
+print(quicksort(arr))
+
+
