83eb5cf838d665e18636404b41a056f445c92740
Arch me <kishudwivedi30@gmail.com>
2025-03-20 13:11:27 +0530

    jkdfgkld

---
+++ grind/binsearch5.py
@@ -0,0 +1,21 @@
+def binsearch(arr, n):
+
+    hi = len(arr) - 1
+    lo = 0
+
+    while lo <= hi:
+        m = (lo + (hi-lo)//2)
+        v = arr[m]
+
+        if v == n:
+            return m
+        elif v > n:
+            hi = m - 1
+        else:
+            lo = m + 1
+
+arr = [3, 5, 7, 9, 11]
+
+n = 5
+
+print(binsearch(arr, n))
