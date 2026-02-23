17bc0fcf2b1e117b2e19928272412b5535236ec5
Arch me <kishudwivedi30@gmail.com>
2026-02-23 14:07:07 +0530

    grp ana

---
+++ graana.py
@@ -0,0 +1,22 @@
+from collections import defaultdict;
+import sys
+
+def groupAna(strs):
+    res = defaultdict(list);
+    for s in strs:
+        count = [0] * 26;
+        for c in s:
+            count[ord(c) - ord("a")] += 1;
+        res[tuple(count)].append(s);
+    return res.values();
+
+def main():
+    args = sys.argv[1:]
+    if not args:
+        print("Please provide input");
+        return;
+
+    results = groupAna(args);
+    print(results);
+
+main();
